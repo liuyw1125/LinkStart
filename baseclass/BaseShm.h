551e9ea46e9367ea75a423edb20e89a6ce846424
overfly_linux <1765074348@qq.com_linux>
2022-07-10 18:19:07 +0800

    [Init BaseShm class]
    Signed-off-by: overfly_linux <1765074348@qq.com_linux>

---
+++ baseclass/BaseShm.h
@@ -0,0 +1,31 @@
+#ifndef BASESHM_H
+#define BASESHM_H
+
+#include <iostream>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <string>
+using namespace std;
+class BaseShm
+{
+public:
+    //通过key打开共享内存
+    BaseShm(int key);
+    //通过传递进来的key创建/打开共享内存
+    BaseShm(int key, int size);
+    //通过路径打开共享内存
+    BaseShm(string name);
+    //通过路径创建/打开共享内存
+    BaseShm(string name, int size);
+    void * mapShm();
+    int unmapShm();
+    int delShm();
+    ~BaseShm();
+private:
+    int getShmID(key_t key, int shmSize, int flag);
+private:
+    int m_shmID;
+protected:
+    void *  m_shmAddr = NULL;
+};
+#endif
